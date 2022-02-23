985f742384d1d8f0f8f376a824955f78b99983ff
Bevin Duncan <bevin.duncan@gmail.com>
2022-02-22 21:09:50 -0800

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return ( 
+    <div id="about">
+      <h2>About Me</h2>
+        <p>"I'm from outer space 👽"</p>
+        <img src={image} alt="I made this"/>
+    </div>
+  )
 }
 
 export default About;
